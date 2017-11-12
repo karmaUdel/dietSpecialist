9f1d56d3b5e6f45afe01e4d656560d971efc93fc
karmaUdel <karma@udel.edu>
2017-11-12 09:30:56 -0500

    Minor feature addition

--- server.py
+++ server.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template,request
 from service import generateResponse
 app = Flask(__name__)
 boilerplateHead = "<html><style>tr:nth-child(even) {color:red;background: #CCC} tr:nth-child(odd) {color:blue;background: #FFF}</style><body style = \"text-align:center; background-color:black; font-size: 30px;\"> <table style = \"text-align:center; margin:1em auto;\"><tr><td style=\"font-size:30px\">"
-boilerplateTail = "</td></tr></table></body></html>"
+boilerplateTail = "</td></tr></table><a href =\"/\">Home</a></body></html>"
 @app.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
